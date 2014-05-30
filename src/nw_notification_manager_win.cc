2f6d279d18f95c2f4a0b4ffda5c0c363453d7f56
Jefry <jefry.ted@vcube.co.jp>
2014-05-30 11:48:23 +0800

    [WIN] implement windows balloon tray notification, with application icon as the tray icon

--- src/nw_notification_manager_win.cc
+++ src/nw_notification_manager_win.cc
@@ -7,27 +7,107 @@
 //
 
 #include "content/nw/src/nw_notification_manager_win.h"
+#include "chrome/browser/status_icons/status_icon.h"
+#include "chrome/browser/status_icons/status_icon_observer.h"
+#include "chrome/browser/status_icons/status_tray.h"
+
+#include "content/public/browser/render_view_host.h"
+#include "content/nw/src/browser/native_window.h"
+#include "content/nw/src/nw_package.h"
+#include "content/nw/src/nw_shell.h"
+
+#include "ui/gfx/image/image.h"
+#include "base/strings/utf_string_conversions.h"
+
 
 namespace nw {
+	class TrayObserver : public StatusIconObserver {
+	public:
+		TrayObserver(NotificationManagerWin* tray)
+			: tray_(tray) {
+		}
+
+		virtual ~TrayObserver() {
+		}
+
+		virtual void OnStatusIconClicked() OVERRIDE{
+
+		}
+		
+		virtual void OnBalloonEvent(int event) OVERRIDE{
+			switch (event) {
+			  case NIN_BALLOONHIDE:
+			  case NIN_BALLOONTIMEOUT:
+				  tray_->ReleaseNotification();
+			}
+		}
+
+		virtual void OnBalloonClicked() OVERRIDE{
+			tray_->DesktopNotificationPostClick();
+			tray_->ReleaseNotification();
+		}
+	private:
+		NotificationManagerWin* tray_;
+	};
+
+	NotificationManagerWin::NotificationManagerWin(){
+		status_tray_ = StatusTray::Create();
+		status_icon_ = nullptr;
+		notification_count_ = 0;
+	}
+
+	bool NotificationManagerWin::ReleaseNotification(){
+		if (notification_count_ > 0){
+			if (notification_count_ == 1) {
+				status_icon_->RemoveObserver(status_observer_);
+				delete status_observer_;
+
+				status_tray_->RemoveStatusIcon(status_icon_);
+				status_icon_ = NULL;
+			}
+			notification_count_--;
+			return true;
+		}
+		return false;
+	}
+
+
+	NotificationManagerWin::~NotificationManagerWin(){
+		ReleaseNotification();
+
+		if (status_tray_){
+			delete status_tray_;
+			status_tray_ = NULL;
+		}
+	}
+
 	bool NotificationManagerWin::AddDesktopNotification(const content::ShowDesktopNotificationHostMsgParams& params,
 		const int render_process_id, const int render_view_id, const bool worker){
-		NOTIFYICONDATA nid = {};
-		nid.cbSize = sizeof(nid);
-		nid.hWnd = NULL;// window_->GetNativeWindow();
-		nid.uID = params.notification_id;
-		nid.uFlags = NIF_INFO | NIF_TIP | NIF_SHOWTIP;// | NIF_ICON;
-		nid.dwInfoFlags = NIIF_LARGE_ICON | NIIF_INFO;//NIIF_USER;
+		content::RenderViewHost* host = content::RenderViewHost::FromID(render_process_id, render_view_id);
+		if (host == nullptr)
+			return false;
 
-		lstrcpyn(nid.szInfoTitle, std::wstring(params.title.begin(), params.title.end()).c_str(), ARRAYSIZE(nid.szInfoTitle));
-		lstrcpyn(nid.szInfo, std::wstring(params.body.begin(), params.body.end()).c_str(), ARRAYSIZE(nid.szInfo));
-		lstrcpyn(nid.szTip, std::wstring(params.body.begin(), params.body.end()).c_str(), ARRAYSIZE(nid.szTip));
+		render_process_id_ = render_process_id;
+		render_view_id_ = render_view_id;
+		notification_id_ = params.notification_id;
 
-		//LoadIconMetric(NULL, MAKEINTRESOURCE(IDI_APPLICATION), LIM_SMALL, &(nid.hIcon));
-		//LoadIconMetric(NULL, MAKEINTRESOURCE(IDI_INFORMATION), LIM_SMALL, &(nid.hBalloonIcon));
-		return Shell_NotifyIcon(NIM_ADD, &nid);
+		content::Shell* shell = content::Shell::FromRenderViewHost(host);
+		nw::Package* package = shell->GetPackage();
+		gfx::Image icon = shell->window()->app_icon();
+
+		if (status_icon_ == nullptr){
+			status_icon_ = status_tray_->CreateStatusIcon(StatusTray::NOTIFICATION_TRAY_ICON,
+				*(shell->window()->app_icon().ToImageSkia()), UTF8ToUTF16(package->GetName()));
+			status_observer_ = new TrayObserver(this);
+			status_icon_->AddObserver(status_observer_);
+		}
+		notification_count_++;
+		status_icon_->DisplayBalloon(icon.IsEmpty() ? gfx::ImageSkia() : *icon.ToImageSkia(), params.title, params.body);
+		return true;
 	}
 
 	bool NotificationManagerWin::CancelDesktopNotification(int render_process_id, int render_view_id, int notification_id){
-		return false;
+		//windows  can only have 1 notification, cannot delete existing notification
+		return true;
 	}
 } // namespace nw
--- src/nw_notification_manager_win.h
+++ src/nw_notification_manager_win.h
@@ -10,13 +10,35 @@
 #define CONTENT_NW_NOTIFICATION_MANAGER_WIN_H_
 
 #include "content/nw/src/nw_notification_manager.h"
+class StatusTray;
+class StatusIcon;
 
 namespace nw {
-
 	class NotificationManagerWin : public NotificationManager{
+		// The global presentation of system tray.
+		StatusTray* status_tray_;
+
+		// The icons on the system tray.
+		StatusIcon* status_icon_;
+		//number of notification in the queue
+		int notification_count_;
+		//decrement the status_icon_count_, if the value is 0 remove the status_icon_ from the tray
+		bool ReleaseNotification();
+
+		// Click observer.
+		friend class TrayObserver;
+		TrayObserver* status_observer_;
+		
+		//variable to store the latest notification data, windows can only show 1 notification
+		int render_process_id_, render_view_id_, notification_id_;
+		//dispatch the click event from the latest notification
+		bool DesktopNotificationPostClick(){
+			return NotificationManager::DesktopNotificationPostClick(render_process_id_, render_view_id_, notification_id_);
+		}
+
 	public:
-		explicit NotificationManagerWin(){}
-		virtual ~NotificationManagerWin(){}
+		explicit NotificationManagerWin();
+		virtual ~NotificationManagerWin();
 		virtual bool AddDesktopNotification(const content::ShowDesktopNotificationHostMsgParams& params,
 											const int render_process_id, const int render_view_id, const bool worker);
 		virtual bool CancelDesktopNotification(int render_process_id, int render_view_id, int notification_id);
