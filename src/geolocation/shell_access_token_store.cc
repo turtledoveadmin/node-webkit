2858d11b291f11822479e671fd9909e99244f72d
Jefry <jefry.ted@vcube.co.jp>
2014-12-08 16:47:16 +0800

    [Geolocation][OSX][WIN] link OSX CoreLocation Framework, add LocationAPI lib and dll supply google's network location provider url, enable the permission by default

--- src/geolocation/shell_access_token_store.cc
+++ src/geolocation/shell_access_token_store.cc
@@ -8,14 +8,26 @@
 #include "base/message_loop/message_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/geolocation_provider.h"
 #include "content/nw/src/shell_browser_context.h"
 
 namespace content {
+  
+void SetPermission() {
+  // nwjs by default allow the location services
+  GeolocationProvider::GetInstance()->UserDidOptIntoLocationServices();
+}
 
 ShellAccessTokenStore::ShellAccessTokenStore(
     content::ShellBrowserContext* shell_browser_context)
     : shell_browser_context_(shell_browser_context),
       system_request_context_(NULL) {
+
+  // post the task to set location permission
+  BrowserThread::PostTask(
+      BrowserThread::UI,
+      FROM_HERE,
+      base::Bind(&SetPermission));
 }
 
 ShellAccessTokenStore::~ShellAccessTokenStore() {
@@ -44,12 +56,12 @@ void ShellAccessTokenStore::RespondOnOriginatingThread(
   // Since content_shell is a test executable, rather than an end user program,
   // we provide a dummy access_token set to avoid hitting the server.
   AccessTokenSet access_token_set;
-  access_token_set[GURL()] = base::ASCIIToUTF16("chromium_content_shell");
+  access_token_set[GURL("https://maps.googleapis.com/maps/api/browserlocation/json?browser=chromium")] = base::ASCIIToUTF16("node_webkit_browserlocation");
   callback.Run(access_token_set, system_request_context_);
 }
 
 void ShellAccessTokenStore::SaveAccessToken(
-                                            const GURL& server_url, const base::string16& access_token) {
+    const GURL& server_url, const base::string16& access_token) {
 }
 
 }  // namespace content
